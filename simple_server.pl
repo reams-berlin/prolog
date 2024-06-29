291b94fec722d6478f0439ec0f753dcebf2621fe
Lindsey Reams <reamslin@gmail.com>
2024-06-29 02:55:49 -0400

    jkjk

--- simple_server.pl
+++ simple_server.pl
@@ -34,11 +34,10 @@
 :- initialization(main, main).
 
 main :-
-    getenv('HOST', Host),
     getenv('PORT', PortStr),
     atom_number(PortStr, Port),  % Convert PortStr to an integer
         format('Starting server on port ~w~n', [Port]),
-    http_server(http_dispatch, [port(Host : Port)]).
+    http_server(http_dispatch, [port(Port)]).
 
 
 
